27319b50b352f437266e204363e13cbc0856862b
g.yoshiyuki <yoshiyukigoubara@gmail.com>
2023-01-14 13:57:21 +0900

    全体修正完了

--- lib/loading.ts
+++ lib/loading.ts
@@ -8,13 +8,13 @@ export const loading = (loadingItems: any, loadingState: any) => {
   window.addEventListener('load', () => {
     // currentが配列に格納されていたら
     if (Array.isArray(loadingItems)) {
-      setTimeout(() => {
+      // setTimeout(() => {
         hideLoading(loadingItems, 'loadingComplete');
-      }, 3000);
+      // }, 3000);
     } else {
-      setTimeout(() => {
+      // setTimeout(() => {
         loadingItems.current.classList.add('loadingComplete');
-      }, 3000);
+      // }, 3000);
     }
     return;
   });
@@ -22,13 +22,13 @@ export const loading = (loadingItems: any, loadingState: any) => {
   // ページ遷移時に実行
   if (loadingState === 'complete') {
     if (Array.isArray(loadingItems)) {
-      setTimeout(() => {
+      // setTimeout(() => {
         hideLoading(loadingItems, 'loadingComplete');
-      }, 1000);
+      // }, 1000);
     } else {
-      setTimeout(() => {
+      // setTimeout(() => {
         loadingItems.current.classList.add('loadingComplete');
-      }, 1000);
+      // }, 1000);
     }
   }
 };
